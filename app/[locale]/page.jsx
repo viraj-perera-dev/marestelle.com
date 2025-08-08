307a519f00020c0ad7d675f8099ab9b3157d7078
Viraj Perera <viraj.perera.development@gmail.com>
2025-08-08 15:25:23 +0200

    update rehydrate bugs

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -41,6 +41,14 @@ export default async function Home({ params }) {
 
     const t = (key) => messages.HomePage?.[key] ?? key;
 
+    if(!messages) {
+      return (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-white transition-colors duration-300">
+          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+        </div>
+      );
+    }
+
     return (
       <>
         <main>
